72f04e018899d102cc8880ede4d04aebc42af382
danverPy <danverdenis@yandex.ru>
2021-04-12 19:06:11 +0300

    Add all method in calculator

--- calculator.py
+++ calculator.py
@@ -2,8 +2,13 @@ import datetime as dt
 
 
 class Records():
-    def __init__(self, amount, comment, date):
-        
+    def __init__(self, amount, comment, date = None):
+        self.amount = amount
+        self.comment = comment
+        if date == None:
+            self.date = dt.date.today()
+        else:
+            self.date = dt.datetime.strptime(date,'%d.%m.%Y').date()
 
 
 class Calculator():
