4d4f36c07dbc33ee275a15c7f0ed7b76b6cfe282
danverPy <danverdenis@yandex.ru>
2021-04-12 18:34:06 +0300

    add Record

--- calculator.py
+++ calculator.py
@@ -1,1 +1,12 @@
+import datetime as dt
 
+
+class Records():
+    def __init__(self, amount, comment, date):
+        
+
+
+class Calculator():
+    def __init__(self, limit):
+        self.limit = limit
+        
